4a4c05700e8eb4a7fef72121d2a195010f8e6bc4
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-22 01:23:47 +0300

    Step 3 - 3-Way data binding

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
 
   angular.module('socially',['angular-meteor']);
 
-  angular.module("socially").controller("PartiesListCtrl", ['$scope',
-    function($scope){
+  angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
+    function($scope, $meteor){
 
-      $scope.parties = [
+      $scope.parties = $meteor.collection(Parties);
+
+    }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties = [
         {'name': 'Dubstep-Free Zone',
           'description': 'Fast just got faster with Nexus S.'},
         {'name': 'All dubstep all the time',
@@ -13,6 +23,8 @@ if (Meteor.isClient) {
         {'name': 'Savage lounging',
           'description': 'Leisure suit required. And only fiercest manners.'}
       ];
-
-    }]);
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert({name: parties[i].name, description: parties[i].description});
+    }
+  });
 }
