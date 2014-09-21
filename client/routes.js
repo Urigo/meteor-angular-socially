45660a673b471d045d267616ab1cc212b7a25bc9
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-22 01:23:47 +0300

    Step 8 - User accounts and permissions

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,13 @@
+angular.module("socially").run(["$rootScope", "$location", function($rootScope, $location) {
+  $rootScope.$on("$stateChangeError", function(event, next, previous, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === "AUTH_REQUIRED") {
+      $location.path("/parties");
+    }
+  });
+}]);
+
 angular.module("socially").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
   function($urlRouterProvider, $stateProvider, $locationProvider){
 
@@ -12,7 +22,12 @@ angular.module("socially").config(['$urlRouterProvider', '$stateProvider', '$loc
       .state('partyDetails', {
         url: '/parties/:partyId',
         templateUrl: 'client/parties/views/party-details.ng.html',
-        controller: 'PartyDetailsCtrl'
+        controller: 'PartyDetailsCtrl',
+        resolve: {
+          "currentUser": ["$meteor", function($meteor){
+            return $meteor.requireUser();
+          }]
+        }
       });
 
     $urlRouterProvider.otherwise("/parties");
