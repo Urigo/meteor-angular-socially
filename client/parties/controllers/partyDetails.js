a1143b7449b4cbf561e4f86a2badf9c631b3c809
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-22 01:23:47 +0300

    Step 12 - Search, sort, pagination and reactive vars

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,8 +1,16 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
   function($scope, $stateParams, $meteor){
 
-    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
-    
+    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+
+    var subscriptionHandle;
+    $meteor.subscribe('parties').then(function(handle) {
+      subscriptionHandle = handle;
+    });
+
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
-}]);
+    $scope.$on('$destroy', function() {
+      subscriptionHandle.stop();
+    });
+  }]);
