20ca8693b8bbb8fd85067bff3cb699e754c3e6cf
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-26 15:02:18 +0300

    step 2 - angular template with repeater

---
+++ app.js
@@ -0,0 +1,22 @@
+if (Meteor.isClient) {
+
+  angular.module('socially',['angular-meteor']);
+
+  Meteor.startup(function () {
+    angular.bootstrap(document, ['socially']);
+  });
+
+  angular.module("socially").controller("PartiesListCtrl", ['$scope',
+    function($scope){
+
+      $scope.parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+
+    }]);
+}
