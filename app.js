7f1d40636386f98ce2f3574e5a4eda0d6cefe1f5
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-28 15:48:48 +0300

    step-5 Routing & Multiple Views

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ Parties = new Mongo.Collection("parties");
 
 if (Meteor.isClient) {
 
-  angular.module('socially',['angular-meteor']);
+  angular.module('socially',['angular-meteor', 'ui.router']);
 
+  angular.module("socially").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+    function($urlRouterProvider, $stateProvider, $locationProvider){
+
+      $locationProvider.html5Mode(true);
+
+      $stateProvider
+        .state('parties', {
+          url: '/parties',
+          templateUrl: 'parties-list.tpl',
+          controller: 'PartiesListCtrl'
+        })
+        .state('partyDetails', {
+          url: '/parties/:partyId',
+          templateUrl: 'party-details.tpl',
+          controller: 'PartyDetailsCtrl'
+        });
+
+      $urlRouterProvider.otherwise("/parties");
+    }]);
+  
   angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorCollection',
     function($scope, $meteorCollection){
 
@@ -12,6 +32,13 @@ if (Meteor.isClient) {
       $scope.remove = function(party){
         $scope.parties.splice( $scope.parties.indexOf(party), 1 );
       };
+
+    }]);
+
+  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams',
+    function($scope, $stateParams){
+
+      $scope.partyId = $stateParams.partyId;
 
     }]);
 }
