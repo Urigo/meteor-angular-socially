29b7f49cd77a3298b2a4541a980c7a36a4cee442
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-28 15:48:48 +0300

    step-4 Adding and removing objects

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ if (Meteor.isClient) {
 
       $collection(Parties).bind($scope, 'parties', true, true);
 
+      $scope.remove = function(party){
+        $scope.parties.splice( $scope.parties.indexOf(party), 1 );
+      };
+
     }]);
 }
 
