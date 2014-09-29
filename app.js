018f1e28dc1c87ecffd2fbf3951021e8537d51f4
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-29 21:13:51 +0300

    step-6 Bind one object

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ if (Meteor.isClient) {
 
     }]);
 
-  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams',
-    function($scope, $stateParams){
+  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$collection',
+    function($scope, $stateParams, $collection){
 
-      $scope.partyId = $stateParams.partyId;
+      $collection(Parties).bindOne($scope, 'party', $stateParams.partyId, true);
 
     }]);
 }
