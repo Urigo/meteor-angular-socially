ff055b72ab775b8d088729687ba4b2950f773938
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-07 20:14:43 +0300

    Step-8 User accounts and permissions

--- model/parties.js
+++ model/parties.js
@@ -1,1 +1,19 @@
 Parties = new Mongo.Collection("parties");
+
+Parties.allow({
+  insert: function (userId, party) {
+    return userId && party.owner === userId;
+  },
+  update: function (userId, party, fields, modifier) {
+    if (userId !== party.owner)
+      return false;
+
+    return true;
+  },
+  remove: function (userId, party) {
+    if (userId !== party.owner)
+      return false;
+
+    return true;
+  }
+});
