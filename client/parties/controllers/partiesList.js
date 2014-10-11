34c7a872324c2e2daf0fec5a9ece43436822d910
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-11 23:56:07 +0300

    Step 11 - Meteor methods with promises

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,9 +1,8 @@
-angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorCollection', '$meteorSubscribe', '$rootScope',
-  function($scope, $meteorCollection, $meteorSubscribe, $rootScope){
-
-    $meteorSubscribe.subscribe('users');
+angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorCollection', '$rootScope', '$meteorMethods',
+  function($scope, $meteorCollection, $rootScope, $meteorMethods){
 
     $scope.parties = $meteorCollection(Parties).subscribe('parties');
+    $scope.users = $meteorCollection(Meteor.users, false).subscribe('users');
 
     $scope.remove = function(party){
       $scope.parties.splice( $scope.parties.indexOf(party), 1 );
@@ -27,5 +26,24 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorColl
           if (owner._id === $rootScope.currentUser._id)
             return "me";
       return owner;
+    };
+
+    $scope.outstandingInvitations = function (party) {
+
+      return _.filter($scope.users, function (user) {
+        return (_.contains(party.invited, user._id) &&
+          !_.findWhere(party.rsvps, {user: user._id}));
+      });
+    };
+
+    $scope.rsvp = function(partyId, rsvp){
+      $meteorMethods.call('rsvp', partyId, rsvp).then(
+        function(data){
+          console.log('success responding', data);
+        },
+        function(err){
+          console.log('failed', err);
+        }
+      );
     };
 }]);
--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,8 +1,19 @@
-angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteorObject', '$meteorCollection',
-  function($scope, $stateParams, $meteorObject, $meteorCollection){
+angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteorObject', '$meteorCollection', '$meteorMethods',
+  function($scope, $stateParams, $meteorObject, $meteorCollection, $meteorMethods){
 
     $scope.party = $meteorObject(Parties, $stateParams.partyId).subscribe('parties');
 
     $scope.users = $meteorCollection(Meteor.users, false).subscribe('users');
+
+    $scope.invite = function(user){
+      $meteorMethods.call('invite', $scope.party._id, user._id).then(
+        function(data){
+          console.log('success inviting', data);
+        },
+        function(err){
+          console.log('failed', err);
+        }
+      );
+    };
 
 }]);
