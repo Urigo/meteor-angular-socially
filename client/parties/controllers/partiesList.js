2a099a4d141f7f345369a2bad7fe323fa040939a
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-13 02:16:35 +0300

    Step 9 - Add publish to users collection

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -3,6 +3,8 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$collection
 
     $collection(Parties).bind($scope, 'parties', true, true);
 
+    $collection(Meteor.users).bind($scope, 'users', false, true);
+
     $scope.remove = function(party){
       $scope.parties.splice( $scope.parties.indexOf(party), 1 );
     };
