32a1c721b90a9d7bfb4026faa6e1e22008f0ee65
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-13 04:13:11 +0300

    Step 9 - Moving the users list to party details

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -3,8 +3,6 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$collection
 
     $collection(Parties).bind($scope, 'parties', true, true);
 
-    $collection(Meteor.users).bind($scope, 'users', false, true);
-
     $scope.remove = function(party){
       $scope.parties.splice( $scope.parties.indexOf(party), 1 );
     };
