b4c18915a345ce461070bea2320f153c77f1af01
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-15 15:56:56 +0300

    Step 10 - Add null check for displayName filter

--- client/parties/filters/displayName.js
+++ client/parties/filters/displayName.js
@@ -1,5 +1,7 @@
 angular.module("socially").filter('displayName', function () {
   return function (user) {
+    if (!user)
+      return;
     if (user.profile && user.profile.name)
       return user.profile.name;
     else if (user.emails)
