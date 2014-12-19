92cafda27949ace8455018dd39a6b27041653335
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-12-19 23:19:31 +0200

    Step 10 - Using and creating AngularJS filters

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -7,4 +7,23 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$collection
       $scope.parties.splice( $scope.parties.indexOf(party), 1 );
     };
 
-}]);
+    $scope.orderProperty = 'name';
+
+    $scope.getUserById = function(userId){
+      return Meteor.users.findOne(userId);
+    };
+
+    $scope.creator = function(party){
+      if (!party)
+        return;
+      var owner = $scope.getUserById(party.owner);
+      if (!owner)
+        return "noboby";
+
+      if ($scope.user)
+        if ($scope.user._id)
+          if (owner._id === $scope.user._id)
+            return "me";
+      return owner;
+    };
+  }]);
