1fd6a8a83e26c98dd5566eb87d603b2ce0798110
Netanel Gilad <netanelgilad@gmail.com>
2015-03-03 20:52:57 +0200

    fix pagination not working because of subscriptions not being stopped.

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,8 +1,16 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
   function($scope, $stateParams, $meteor){
 
-    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
-    
+    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+
+    var subscruptionHandle;
+    $meteor.subscribe('parties').then(function(handle) {
+      subscruptionHandle = handle;
+    });
+
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
+    $scope.$on('$destroy', function() {
+      subscruptionHandle.stop();
+    });
 }]);
