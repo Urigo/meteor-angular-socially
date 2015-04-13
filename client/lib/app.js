e316f4cd80f36ded91c91d75776e3c9825bd0032
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-04-13 15:31:44 +0300

    Step 16 - Google Maps

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,4 +1,9 @@
-angular.module('socially',['angular-meteor', 'ui.router', 'angularUtils.directives.dirPagination']);
+angular.module('socially',[
+  'angular-meteor',
+  'ui.router',
+  'angularUtils.directives.dirPagination',
+  'uiGmapgoogle-maps'
+]);
 
 function onReady() {
   angular.bootstrap(document, ['socially']);
