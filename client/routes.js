ac0a81731c910ef7b1854deccdd8d9d79aaf4a3b
oshai <ohadshai@gmail.com>
2015-06-06 13:48:30 +0300

    fix according to issue #283

--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,9 @@
-angular.module("socially").run(["$rootScope", "$location", function($rootScope, $location) {
+angular.module("socially").run(["$rootScope", "$location", function($rootScope, $state) {
   $rootScope.$on("$stateChangeError", function(event, next, previous, error) {
     // We can catch the error thrown when the $requireUser promise is rejected
     // and redirect the user back to the main page
     if (error === "AUTH_REQUIRED") {
-      $location.path("/parties");
+      $state.go("/parties");
     }
   });
 }]);
