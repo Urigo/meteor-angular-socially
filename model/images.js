e4f37d8796f9bddc77e0a965223d35fcef6e85ca
Dotan Simha <dotansimha@gmail.com>
2015-06-28 21:44:13 +0300

    Add step20 - image upload to the project

---
+++ model/images.js
@@ -0,0 +1,31 @@
+Images = new FS.Collection("images", {
+  stores: [
+    new FS.Store.GridFS("original")
+  ],
+  filter: {
+    allow: {
+      contentTypes: ['image/*']
+    }
+  }
+});
+
+if (Meteor.isServer) {
+  Images.allow({
+    insert: function (userId) {
+      return (userId ? true : false);
+    },
+    remove: function (userId) {
+      return (userId ? true : false);
+    },
+    download: function () {
+      return true;
+    },
+    update: function (userId) {
+      return (userId ? true : false);
+    }
+  });
+
+  Meteor.publish('images', function() {
+    return Images.find({});
+  });
+}
