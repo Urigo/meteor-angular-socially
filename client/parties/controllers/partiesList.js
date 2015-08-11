1c908f099c85fce42632c2f049ceab1d5a93ad6f
Hadar Landao <hlandao@gmail.com>
2015-08-11 10:47:08 +0300

    cleanup

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,121 +1,173 @@
 angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '$rootScope', '$state', '$modal',
-  function($scope, $meteor, $rootScope, $state, $modal){
+    function ($scope, $meteor, $rootScope, $state, $modal) {
 
-    $scope.page = 1;
-    $scope.perPage = 3;
-    $scope.sort = { name: 1 };
-    $scope.orderProperty = '1';
+        $scope.page = 1;
+        $scope.perPage = 3;
+        $scope.sort = {name: 1};
+        $scope.orderProperty = '1';
 
-    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
-    
-    $scope.parties = $meteor.collection(function() {
-      return Parties.find({}, {
-        sort : $scope.getReactively('sort')
-      });
-    });
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
-    $meteor.autorun($scope, function() {
-      $meteor.subscribe('parties', {
-        limit: parseInt($scope.getReactively('perPage')),
-        skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
-        sort: $scope.getReactively('sort')
-      }, $scope.getReactively('search')).then(function() {
-        $scope.partiesCount = $meteor.object(Counts ,'numberOfParties', false);
-
-        $scope.parties.forEach( function (party) {
-          party.onClicked = function () {
-            $state.go('partyDetails', {partyId: party._id});
-          };
+        $scope.parties = $meteor.collection(function () {
+            return Parties.find({}, {
+                sort: $scope.getReactively('sort')
+            });
         });
 
+        $meteor.autorun($scope, function () {
+            $meteor.subscribe('parties', {
+                limit: parseInt($scope.getReactively('perPage')),
+                skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
+                sort: $scope.getReactively('sort')
+            }, $scope.getReactively('search')).then(function () {
+                $scope.partiesCount = $meteor.object(Counts, 'numberOfParties', false);
 
-        var styles1 = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}];
-        var styles2 = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}];
+                $scope.parties.forEach(function (party) {
+                    party.onClicked = function () {
+                        $state.go('partyDetails', {partyId: party._id});
+                    };
+                });
 
-        $scope.map = {
-          center: {
-            latitude: 45,
-            longitude: -73
-          },
-          options : {
-            styles : styles2,
-            maxZoom : 10
-          },
-          zoom: 8
+
+                var styles1 = [{
+                    "featureType": "landscape.natural",
+                    "elementType": "geometry.fill",
+                    "stylers": [{"visibility": "on"}, {"color": "#e0efef"}]
+                }, {
+                    "featureType": "poi",
+                    "elementType": "geometry.fill",
+                    "stylers": [{"visibility": "on"}, {"hue": "#1900ff"}, {"color": "#c0e8e8"}]
+                }, {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [{"lightness": 100}, {"visibility": "simplified"}]
+                }, {
+                    "featureType": "road",
+                    "elementType": "labels",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [{"visibility": "on"}, {"lightness": 700}]
+                }, {"featureType": "water", "elementType": "all", "stylers": [{"color": "#7dcdcd"}]}];
+                var styles2 = [{
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [{"color": "#444444"}]
+                }, {
+                    "featureType": "landscape",
+                    "elementType": "all",
+                    "stylers": [{"color": "#f2f2f2"}]
+                }, {
+                    "featureType": "poi",
+                    "elementType": "all",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "road",
+                    "elementType": "all",
+                    "stylers": [{"saturation": -100}, {"lightness": 45}]
+                }, {
+                    "featureType": "road.highway",
+                    "elementType": "all",
+                    "stylers": [{"visibility": "simplified"}]
+                }, {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.icon",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "transit",
+                    "elementType": "all",
+                    "stylers": [{"visibility": "off"}]
+                }, {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [{"color": "#46bcec"}, {"visibility": "on"}]
+                }];
+
+                $scope.map = {
+                    center: {
+                        latitude: 45,
+                        longitude: -73
+                    },
+                    options: {
+                        styles: styles2,
+                        maxZoom: 10
+                    },
+                    zoom: 8
+                };
+            });
+        });
+
+        $scope.remove = function (party) {
+            $scope.parties.splice($scope.parties.indexOf(party), 1);
         };
-      });
-    });
 
-    $scope.remove = function(party){
-      $scope.parties.splice( $scope.parties.indexOf(party), 1 );
-    };
+        $scope.pageChanged = function (newPage) {
+            $scope.page = newPage;
+        };
 
-    $scope.pageChanged = function(newPage) {
-      $scope.page = newPage;
-    };
+        $scope.$watch('orderProperty', function () {
+            if ($scope.orderProperty)
+                $scope.sort = {name: parseInt($scope.orderProperty)};
+        });
 
-    $scope.$watch('orderProperty', function(){
-      if ($scope.orderProperty)
-        $scope.sort = {name: parseInt($scope.orderProperty)};
-    });
+        $scope.getUserById = function (userId) {
+            return Meteor.users.findOne(userId);
+        };
 
-    $scope.getUserById = function(userId){
-      return Meteor.users.findOne(userId);
-    };
+        $scope.creator = function (party) {
+            if (!party)
+                return;
+            var owner = $scope.getUserById(party.owner);
+            if (!owner)
+                return "nobody";
 
-    $scope.creator = function(party){
-      if (!party)
-        return;
-      var owner = $scope.getUserById(party.owner);
-      if (!owner)
-        return "nobody";
+            if ($rootScope.currentUser)
+                if ($rootScope.currentUser._id)
+                    if (owner._id === $rootScope.currentUser._id)
+                        return "me";
 
-      if ($rootScope.currentUser)
-        if ($rootScope.currentUser._id)
-          if (owner._id === $rootScope.currentUser._id)
-            return "me";
+            return owner;
+        };
 
-      return owner;
-    };
+        $scope.rsvp = function (partyId, rsvp) {
+            $meteor.call('rsvp', partyId, rsvp).then(
+                function (data) {
+                    console.log('success responding', data);
+                },
+                function (err) {
+                    console.log('failed', err);
+                }
+            );
+        };
 
-    $scope.rsvp = function(partyId, rsvp){
-      $meteor.call('rsvp', partyId, rsvp).then(
-        function(data){
-          console.log('success responding', data);
-        },
-        function(err){
-          console.log('failed', err);
+        $scope.openAddNewPartyModal = function (size) {
+
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'client/parties/views/add-new-party-modal.ng.html',
+                controller: 'AddNewPartyCtrl',
+                resolve: {
+                    parties: function () {
+                        return $scope.parties;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function () {
+            }, function () {
+            });
+        };
+
+
+        $scope.isRSVP = function (rsvp, party) {
+            if (!$rootScope.currentUser._id) return false;
+            var rsvpIndex = party.myRsvpIndex;
+            rsvpIndex = rsvpIndex || _.indexOf(_.pluck(party.rsvps, 'user'), $rootScope.currentUser._id);
+
+            if (rsvpIndex !== -1) {
+                party.myRsvpIndex = rsvpIndex;
+                return party.rsvps[rsvpIndex].rsvp === rsvp;
+            }
         }
-      );
-    };
-
-    $scope.openAddNewPartyModal = function (size) {
-
-      var modalInstance = $modal.open({
-        animation: true,
-        templateUrl: 'client/parties/views/add-new-party-modal.ng.html',
-        controller: 'AddNewPartyCtrl',
-        resolve : {
-          parties : function(){
-            return $scope.parties;
-          }
-        }
-      });
-
-      modalInstance.result.then(function () {
-      }, function () {
-      });
-    };
-
-
-    $scope.isRSVP = function(rsvp, party){
-      if(!$rootScope.currentUser._id) return false;
-      var rsvpIndex = party.myRsvpIndex;
-      rsvpIndex = rsvpIndex || _.indexOf(_.pluck(party.rsvps, 'user'), $rootScope.currentUser._id);
-
-      if(rsvpIndex !== -1){
-        party.myRsvpIndex = rsvpIndex;
-        return party.rsvps[rsvpIndex].rsvp === rsvp;
-      }
-    }
-}]);
+    }]);
--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,70 +1,70 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
-  function($scope, $stateParams, $meteor){
+    function ($scope, $stateParams, $meteor) {
 
-    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+        $scope.party = $meteor.object(Parties, $stateParams.partyId);
 
-    var subscriptionHandle;
-    $meteor.subscribe('parties').then(function(handle) {
-      subscriptionHandle = handle;
-    });
+        var subscriptionHandle;
+        $meteor.subscribe('parties').then(function (handle) {
+            subscriptionHandle = handle;
+        });
 
-    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
-    $scope.invite = function(user){
-      $meteor.call('invite', $scope.party._id, user._id).then(
-        function(data){
-          console.log('success inviting', data);
-        },
-        function(err){
-          console.log('failed', err);
-        }
-      );
-    };
+        $scope.invite = function (user) {
+            $meteor.call('invite', $scope.party._id, user._id).then(
+                function (data) {
+                    console.log('success inviting', data);
+                },
+                function (err) {
+                    console.log('failed', err);
+                }
+            );
+        };
 
-    $scope.$on('$destroy', function() {
-      subscriptionHandle.stop();
-    });
-    
-    $scope.canInvite = function (){
-        if (!$scope.party)
-          return false;
-  
-        return !$scope.party.public &&
-          $scope.party.owner === Meteor.userId();
-    };
+        $scope.$on('$destroy', function () {
+            subscriptionHandle.stop();
+        });
 
-    $scope.map = {
-      center: {
-        latitude: 45,
-        longitude: -73
-      },
-      zoom: 8,
-      events: {
-        click: function (mapModel, eventName, originalEventArgs) {
-          if (!$scope.party)
-            return;
+        $scope.canInvite = function () {
+            if (!$scope.party)
+                return false;
 
-          if (!$scope.party.location)
-            $scope.party.location = {};
+            return !$scope.party.public &&
+                $scope.party.owner === Meteor.userId();
+        };
 
-          $scope.party.location.latitude = originalEventArgs[0].latLng.lat();
-          $scope.party.location.longitude = originalEventArgs[0].latLng.lng();
-          //scope apply required because this event handler is outside of the angular domain
-          $scope.$apply();
-        }
-      },
-      marker: {
-        options: { draggable: true },
-        events: {
-          dragend: function (marker, eventName, args) {
-            if (!$scope.party.location)
-              $scope.party.location = {};
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8,
+            events: {
+                click: function (mapModel, eventName, originalEventArgs) {
+                    if (!$scope.party)
+                        return;
 
-            $scope.party.location.latitude = marker.getPosition().lat();
-            $scope.party.location.longitude = marker.getPosition().lng();
-          }
-        }
-      }
-    };
+                    if (!$scope.party.location)
+                        $scope.party.location = {};
 
-  }]);
+                    $scope.party.location.latitude = originalEventArgs[0].latLng.lat();
+                    $scope.party.location.longitude = originalEventArgs[0].latLng.lng();
+                    //scope apply required because this event handler is outside of the angular domain
+                    $scope.$apply();
+                }
+            },
+            marker: {
+                options: {draggable: true},
+                events: {
+                    dragend: function (marker, eventName, args) {
+                        if (!$scope.party.location)
+                            $scope.party.location = {};
+
+                        $scope.party.location.latitude = marker.getPosition().lat();
+                        $scope.party.location.longitude = marker.getPosition().lng();
+                    }
+                }
+            }
+        };
+
+    }]);
