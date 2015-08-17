5d43d7c366cf26198e3c9a58a691dd33865ce600
Dotan Simha <dotansimha@gmail.com>
2015-08-17 19:47:13 +0300

    Step 6.5: Add logic to the controller

--- app.ng.js
+++ app.ng.js
@@ -36,5 +36,13 @@ if (Meteor.isClient) {
 
   angular.module("socially").controller("PartyDetailsCtrl", function($scope, $stateParams, $meteor) {
     $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+    $scope.save = function() {
+      $scope.party.save();
+    };
+
+    $scope.reset = function() {
+      $scope.party.reset();
+    };
   });
 }
