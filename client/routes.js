093be506f35f0bf8b5291c0bb4704b33c0a23ce5
Dotan Simha <dotansimha@gmail.com>
2015-08-17 20:41:38 +0300

    Step 8.10: Handle case that user is not logged in

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,13 @@
+angular.module("socially").run(['$rootScope', '$state', function($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('parties');
+    }
+  });
+}]);
+
 angular.module("socially").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
   function ($urlRouterProvider, $stateProvider, $locationProvider) {
 
