ec1a9a6079eb6d5d5845d35875a8feeed95d1241
Dotan Simha <dotansimha@gmail.com>
2015-08-17 21:52:39 +0300

    Step 12.21: Add the search param to the subscribe

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -17,7 +17,7 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
         limit: parseInt($scope.getReactively('perPage')),
         skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
         sort: $scope.getReactively('sort')
-      }).then(function(){
+      }, $scope.getReactively('search')).then(function() {
         $scope.partiesCount = $meteor.object(Counts ,'numberOfParties', false);
       });
     });
