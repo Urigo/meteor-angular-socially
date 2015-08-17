2f370ac092db0ef63a65e2f7086e999bcf1667af
Dotan Simha <dotansimha@gmail.com>
2015-08-17 18:40:05 +0300

    Step 3.3: Add a dependency to $meteor

--- app.ng.js
+++ app.ng.js
@@ -3,7 +3,7 @@ Parties = new Mongo.Collection("parties");
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor']);
 
-  angular.module('socially').controller('PartiesListCtrl',function ($scope) {
+  angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
     $scope.parties = $meteor.collection(Parties);
   });
 }
