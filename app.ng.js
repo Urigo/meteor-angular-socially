4b7fff786aca233025bb99eef47bcbb4b939cec0
Dotan Simha <dotansimha@gmail.com>
2015-08-17 19:16:01 +0300

    Step 5.7: Add routes definitions

--- app.ng.js
+++ app.ng.js
@@ -1,6 +1,25 @@
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor', 'ui.router']);
 
+  angular.module('socially').config(function($urlRouterProvider, $stateProvider, $locationProvider){
+
+      $locationProvider.html5Mode(true);
+
+      $stateProvider
+        .state('parties', {
+          url: '/parties',
+          templateUrl: 'parties-list.ng.html',
+          controller: 'PartiesListCtrl'
+        })
+        .state('partyDetails', {
+          url: '/parties/:partyId',
+          templateUrl: 'party-details.ng.html',
+          controller: 'PartyDetailsCtrl'
+        });
+
+      $urlRouterProvider.otherwise("/parties");
+    });
+  
   angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
     $scope.parties = $meteor.collection(Parties);
 
