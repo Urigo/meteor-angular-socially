ec82ab47fcf59ca7fde2c325001a7e6444ba9d46
Dotan Simha <dotansimha@gmail.com>
2015-08-17 22:35:37 +0300

    Step 14.11: Add users collection

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -6,7 +6,7 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '
     $scope.sort = { name: 1 };
     $scope.orderProperty = '1';
 
-    $scope.$meteorSubscribe('users');
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
     $scope.parties = $meteor.collection(function() {
       return Parties.find({}, {
