ca289953534ec38c9aa0ba0d83f7600ca01d86db
Dotan Simha <dotansimha@gmail.com>
2015-08-17 18:39:33 +0300

    Step 3.2: Add the $meteor.collection to the $scope

--- app.ng.js
+++ app.ng.js
@@ -3,20 +3,7 @@ Parties = new Mongo.Collection("parties");
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor']);
 
-  angular.module('socially').controller('PartiesListCtrl', function ($scope) {
-    $scope.parties = [
-      {
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'
-      },
-      {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!'
-      },
-      {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'
-      }
-    ];
+  angular.module('socially').controller('PartiesListCtrl',function ($scope) {
+    $scope.parties = $meteor.collection(Parties);
   });
 }
