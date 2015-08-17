e906fd64f51f6e7dcbb24ef15f9b22da3751b7c7
Dotan Simha <dotansimha@gmail.com>
2015-08-17 18:53:55 +0300

    Step 4.5: Add remove logic to the controller

--- app.ng.js
+++ app.ng.js
@@ -3,5 +3,9 @@ if (Meteor.isClient) {
 
   angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
     $scope.parties = $meteor.collection(Parties);
+
+    $scope.remove = function(party){
+      $scope.parties.splice($scope.parties.indexOf(party), 1);
+    };
   });
 }
