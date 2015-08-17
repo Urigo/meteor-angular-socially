39d734ee37f804c83f6f58af116b5262a09c8546
Dotan Simha <dotansimha@gmail.com>
2015-08-17 21:50:07 +0300

    Step 12.21: Add the search logic

--- server/parties.js
+++ server/parties.js
@@ -1,5 +1,9 @@
 Meteor.publish("parties", function (options, searchString) {
+  if (searchString == null)
+    searchString = '';
+
   Counts.publish(this, 'numberOfParties', Parties.find({
+    'name' : { '$regex' : '.*' + searchString || '' + '.*', '$options' : 'i' },
     $or:[
       {$and:[
         {'public': true},
@@ -12,6 +16,7 @@ Meteor.publish("parties", function (options, searchString) {
     ]}), { noReady: true });
 
   return Parties.find({
+    'name' : { '$regex' : '.*' + searchString || '' + '.*', '$options' : 'i' },
     $or:[
       {$and:[
         {"public": true},
