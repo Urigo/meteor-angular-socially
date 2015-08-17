5206b5785ed02c07bc81c3e3a632fd015a82bbd8
Dotan Simha <dotansimha@gmail.com>
2015-08-17 21:08:09 +0300

    Step 9.8: Subscribe to the parties collection

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,5 +1,5 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
   function ($scope, $stateParams, $meteor) {
-    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
   }]);
