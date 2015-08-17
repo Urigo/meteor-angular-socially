f6aeb2c19ef98f67d0d7346088a7f348a5444185
Dotan Simha <dotansimha@gmail.com>
2015-08-17 20:50:23 +0300

    Step 9.2: Usage of publish method

---
+++ server/parties.js
@@ -0,0 +1,13 @@
+Meteor.publish("parties", function () {
+  return Parties.find({
+    $or:[
+      {$and:[
+        {"public": true},
+        {"public": {$exists: true}}
+      ]},
+      {$and:[
+        {owner: this.userId},
+        {owner: {$exists: true}}
+      ]}
+    ]});
+});
