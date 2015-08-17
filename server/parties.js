a961bc3cfa69f34652513eaed7c585815e734200
Dotan Simha <dotansimha@gmail.com>
2015-08-17 21:38:00 +0300

    Step 12.12: Add parties count publish

--- server/parties.js
+++ server/parties.js
@@ -1,4 +1,16 @@
 Meteor.publish("parties", function (options) {
+  Counts.publish(this, 'numberOfParties', Parties.find({
+    $or:[
+      {$and:[
+        {'public': true},
+        {'public': {$exists: true}}
+      ]},
+      {$and:[
+        {owner: this.userId},
+        {owner: {$exists: true}}
+      ]}
+    ]}), { noReady: true });
+
   return Parties.find({
     $or:[
       {$and:[
