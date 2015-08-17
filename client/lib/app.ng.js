c0091f5195398081a6949c648798a4cae09fb949
Dotan Simha <dotansimha@gmail.com>
2015-08-17 21:13:54 +0300

    Step 11.1: Update the module initialization

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,1 +1,12 @@
 angular.module('socially',['angular-meteor', 'ui.router']);
+
+function onReady() {
+  angular.bootstrap(document, ['socially'], {
+    strictDi: true
+  });
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
