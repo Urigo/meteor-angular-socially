383379a3d0b1203267246919b8a29c3c01806a00
Dotan Simha <dotansimha@gmail.com>
2015-08-17 21:55:28 +0300

    Step 12.24: Remove the subscription from the object

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,5 +1,5 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
   function ($scope, $stateParams, $meteor) {
-    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+    $scope.party = $meteor.object(Parties, $stateParams.partyId);
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
   }]);
