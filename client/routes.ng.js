7d156102e4b94612723f0e7eae10cc40e6dd994e
Dotan Simha <dotansimha@gmail.com>
2015-08-17 20:06:09 +0300

    Step 7.4: client/routes.ng.js

---
+++ client/routes.ng.js
@@ -0,0 +1,17 @@
+angular.module("socially").config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('parties', {
+      url: '/parties',
+      templateUrl: 'parties-list.ng.html',
+      controller: 'PartiesListCtrl'
+    })
+    .state('partyDetails', {
+      url: '/parties/:partyId',
+      templateUrl: 'party-details.ng.html',
+      controller: 'PartyDetailsCtrl'
+    });
+
+  $urlRouterProvider.otherwise("/parties");
+});
