0d9d4bee404823cd0b82148c787a0dc8044325b4
Dotan Simha <dotansimha@gmail.com>
2015-08-17 20:53:15 +0300

    Step 9.3: Use "subscribe" function on the scope

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -1,7 +1,7 @@
 angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
   function($scope, $meteor){
 
-    $scope.parties = $meteor.collection(Parties);
+    $scope.parties = $meteor.collection(Parties).subscribe('parties');
 
     $scope.remove = function(party){
       $scope.parties.splice( $scope.parties.indexOf(party), 1 );
