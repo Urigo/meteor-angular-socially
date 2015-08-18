13a95dacb5a9c56b125ba8b87c3afb1d42e304bb
Dotan Simha <dotansimha@gmail.com>
2015-08-18 18:37:08 +0300

    Step 20.10: Change the logic of addImages

--- client/parties/controllers/addPhoto.js
+++ client/parties/controllers/addPhoto.js
@@ -2,7 +2,19 @@ angular.module("socially").controller("AddPhotoCtrl", ['$scope', '$meteor', '$ro
   function($scope, $meteor, $rootScope, $state, $mdDialog) {
     $scope.addImages = function (files) {
       if (files.length > 0) {
-        $scope.images.save(files[0]);
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+          $scope.$apply(function() {
+            $scope.imgSrc = e.target.result;
+            $scope.myCroppedImage = '';
+          });
+        };
+
+        reader.readAsDataURL(files[0]);
+      }
+      else {
+        $scope.imgSrc = undefined;
       }
     };
 
