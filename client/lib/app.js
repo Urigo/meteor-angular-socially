458dee19dc60972a32dfaa9962a38c30f910d8db
Dotan Simha <dotansimha@gmail.com>
2015-09-09 14:47:43 +0300

    Step 17.10: Add dependency for ui-bootstrap

--- client/lib/app.js
+++ client/lib/app.js
@@ -2,7 +2,8 @@ angular.module('socially',[
   'angular-meteor',
   'ui.router',
   'angularUtils.directives.dirPagination',
-  'uiGmapgoogle-maps'
+  'uiGmapgoogle-maps',
+  'ui.bootstrap'
 ]);
 function onReady() {
   angular.bootstrap(document, ['socially']);
