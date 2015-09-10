e03019a479e50ffc7763536764f76d6e7c310e09
Dotan Simha <dotansimha@gmail.com>
2015-09-10 18:51:12 +0300

    Step 18.15: Create login controller

---
+++ client/users/controllers/login.js
@@ -0,0 +1,23 @@
+angular.module("socially").controller("LoginCtrl", ['$meteor', '$state',
+  function ($meteor, $state) {
+    var vm = this;
+
+    vm.credentials = {
+      email: '',
+      password: ''
+    };
+
+    vm.error = '';
+
+    vm.login = function () {
+      $meteor.loginWithPassword(vm.credentials.email, vm.credentials.password).then(
+        function () {
+          $state.go('parties');
+        },
+        function (err) {
+          vm.error = 'Login error - ' + err;
+        }
+      );
+    };
+  }
+]);
