44e744789ffe1d8acb00900a2777841a123a14c6
Dotan Simha <dotansimha@gmail.com>
2015-09-11 11:09:03 +0300

    Step 20.2: Add the collectionFs to the controller

--- client/parties/controllers/partiesList.js
+++ client/parties/controllers/partiesList.js
@@ -7,6 +7,7 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '
     $scope.orderProperty = '1';
 
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+    $scope.images = $meteor.collectionFS(Images, false, Images).subscribe('images');
 
     $scope.parties = $meteor.collection(function() {
       return Parties.find({}, {
