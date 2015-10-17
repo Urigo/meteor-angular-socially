be558d7de9f9e03d7dd870a5b910ce699898d335
Dotan Simha <dotansimha@gmail.com>
2015-10-17 17:52:46 +0300

    Step 9.7: Add subscription to users

--- client/parties/controllers/partyDetails.ng.js
+++ client/parties/controllers/partyDetails.ng.js
@@ -1,3 +1,4 @@
 angular.module("socially").controller("PartyDetailsCtrl", function ($scope, $stateParams, $meteor) {
   $scope.party = $meteor.object(Parties, $stateParams.partyId);
+  $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 });
