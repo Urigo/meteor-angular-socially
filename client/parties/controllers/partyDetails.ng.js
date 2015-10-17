4d1d14ed91fa7a4613f1e5b33f9288df809c5dd6
Dotan Simha <dotansimha@gmail.com>
2015-10-17 19:11:27 +0300

    Step 14.3: Add invite method to the client

--- client/parties/controllers/partyDetails.ng.js
+++ client/parties/controllers/partyDetails.ng.js
@@ -2,4 +2,15 @@ angular.module("socially").controller("PartyDetailsCtrl", function ($scope, $sta
   $scope.party = $meteor.object(Parties, $stateParams.partyId);
   $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
   $scope.$meteorSubscribe('parties');
+
+  $scope.invite = function(user){
+    $meteor.call('invite', $scope.party._id, user._id).then(
+      function(data){
+        console.log('success inviting', data);
+      },
+      function(err){
+        console.log('failed', err);
+      }
+    );
+  };
 });
