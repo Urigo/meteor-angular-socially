d98b0aeba790ae73dc21ac948278a0851df79904
Dotan Simha <dotansimha@gmail.com>
2015-10-17 18:18:27 +0300

    Step 8.10: Add authentication logic to the routes

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -1,3 +1,13 @@
+angular.module("socially").run(function ($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('parties');
+    }
+  });
+});
+
 angular.module("socially").config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
