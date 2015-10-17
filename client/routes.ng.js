548ddaeaacdb642cb642520e0f0c9f38b84c7424
Dotan Simha <dotansimha@gmail.com>
2015-10-17 19:58:20 +0300

    Step 18.14: Add custom auth routes

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -22,8 +22,38 @@ angular.module("socially").config(function ($urlRouterProvider, $stateProvider, 
       templateUrl: 'client/parties/views/party-details.ng.html',
       controller: 'PartyDetailsCtrl',
       resolve: {
-        "currentUser": function ($meteor) {
+        "currentUser": function($meteor){
           return $meteor.requireUser();
+        }
+      }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'client/users/views/login.ng.html',
+      controller: 'LoginCtrl',
+      controllerAs: 'lc'
+    })
+    .state('register',{
+      url: '/register',
+      templateUrl: 'client/users/views/register.ng.html',
+      controller: 'RegisterCtrl',
+      controllerAs: 'rc'
+    })
+    .state('resetpw', {
+      url: '/resetpw',
+      templateUrl: 'client/users/views/reset-password.ng.html',
+      controller: 'ResetCtrl',
+      controllerAs: 'rpc'
+    })
+    .state('logout', {
+      url: '/logout',
+      resolve: {
+        "logout": function($meteor, $state) {
+          return $meteor.logout().then(function(){
+            $state.go('parties');
+          }, function(err){
+            console.log('logout error - ', err);
+          });
         }
       }
     });
