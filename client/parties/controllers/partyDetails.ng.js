a99d2d02485a4d3b2169b286c6d56d7d0360b130
Dotan Simha <dotansimha@gmail.com>
2015-10-17 18:54:58 +0300

    Step 12.25: Add $scope subscription to the collection

--- client/parties/controllers/partyDetails.ng.js
+++ client/parties/controllers/partyDetails.ng.js
@@ -1,4 +1,5 @@
 angular.module("socially").controller("PartyDetailsCtrl", function ($scope, $stateParams, $meteor) {
   $scope.party = $meteor.object(Parties, $stateParams.partyId);
   $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+  $scope.$meteorSubscribe('parties');
 });
