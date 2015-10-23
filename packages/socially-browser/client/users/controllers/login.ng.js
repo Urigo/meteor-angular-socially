5e4b81be632e81919d88eddc3a46e0ad3f389312
Tally Barak <tally.barak@gmail.com>
2015-10-23 21:52:15 +0300

    improving user windows look

--- packages/socially-browser/client/users/controllers/login.ng.js
+++ packages/socially-browser/client/users/controllers/login.ng.js
@@ -2,6 +2,12 @@ angular.module("socially.browser").controller("LoginCtrl", ['$meteor', '$state',
   function ($meteor, $state) {
     var vm = this;
 
+  vm.providers = [
+    {name: 'Facebook', icon: 'icons/facebook.svg', color: '#3F62B4'},
+    {name: 'Google', icon:'icons/google.svg',color: '#DC4A38'},
+    {name: 'Twitter',icon: 'icons/twitter.svg',color: '#27AAE2'}
+  ];
+
     vm.credentials = {
       email: '',
       password: ''
