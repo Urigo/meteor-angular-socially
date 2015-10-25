96082b328d41fc70653879e7467be35f6b407fdc
Dotan Simha <dotansimha@gmail.com>
2015-10-25 21:14:52 +0200

    Step 21.8: Include actual files in the package

--- packages/socially-mobile/package.js
+++ packages/socially-mobile/package.js
@@ -10,6 +10,15 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.0.1');
   api.use('ecmascript');
   api.use('angular');
+
+  api.addFiles([
+    'client/lib/module.ng.js',
+    'client/routes.ng.js'
+  ], 'web.cordova');
+
+  api.addAssets([
+    'client/users/views/login-mobile.ng.html'
+  ], 'web.cordova');
 });
 
 Package.onTest(function(api) {
