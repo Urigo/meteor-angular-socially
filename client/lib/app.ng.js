b7b5d2340a2ed139bd4832d0588e504a5a4b91f9
Dotan Simha <dotansimha@gmail.com>
2015-10-25 21:17:34 +0200

    Step 21.10: Add modules isolation

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,4 +1,4 @@
-angular.module('socially',[
+var modulesToLoad = [
   'angular-meteor',
   'ui.router',
   'angularUtils.directives.dirPagination',
@@ -8,7 +8,13 @@ angular.module('socially',[
   'ngImgCrop',
   'xeditable',
   'angular-sortable-view'
-]);
+];
+
+if (Meteor.isCordova) {
+  modulesToLoad = modulesToLoad.concat(['socially.mobile']);
+}
+
+angular.module('socially', modulesToLoad);
 
 var themeIcons = ['$mdIconProvider' , function ($mdIconProvider) {
 
