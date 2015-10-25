49a9ccdc086f99299fc8f80a8cf29ac4a5c9763d
Dotan Simha <dotansimha@gmail.com>
2015-10-25 21:18:26 +0200

    Step 21.12: Add the socially-browser module with similar logic

---
+++ packages/socially-browser/package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'socially-browser',
+  version: '0.0.1',
+  summary: '',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  api.use('ecmascript');
+  api.use('angular');
+
+  api.addFiles([
+    'client/lib/module.ng.js',
+    'client/routes.ng.js'
+  ], 'web.browser');
+
+  api.addAssets([
+    'client/users/views/login-browser.ng.html'
+  ], 'web.browser');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('socially-mobile');
+});
