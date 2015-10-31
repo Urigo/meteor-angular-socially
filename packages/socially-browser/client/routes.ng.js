6cda760f898f834f91cdb6f0eb6086d63f4974ba
Dotan Simha <dotansimha@gmail.com>
2015-10-31 19:39:37 +0200

    Step 22.3: Move the parties list state and add the main state

--- packages/socially-browser/client/routes.ng.js
+++ packages/socially-browser/client/routes.ng.js
@@ -1,8 +1,27 @@
-angular.module('socially.browser').config(function($stateProvider) {
-  $stateProvider.state('login', {
-    url: '/login',
-    templateUrl: '/packages/socially-browser/client/users/views/login-browser.ng.html',
-    controller: 'LoginCtrl',
-    controllerAs: 'lc'
-  });
+angular.module('socially.browser').config(function ($stateProvider) {
+  $stateProvider
+    .state('socially.login', {
+      url: '/login',
+      views: {
+        main: {
+          templateUrl: '/packages/socially-browser/client/users/views/login-browser.ng.html',
+          controller: 'LoginCtrl',
+          controllerAs: 'lc'
+        }
+      }
+    })
+    .state('socially', {
+      url: '/socially',
+      templateUrl: '/packages/socially-browser/client/browser-main.ng.html',
+      abstract: true
+    })
+    .state('socially.parties', {
+      url: '/parties',
+      views: {
+        main: {
+          templateUrl: '/packages/socially-browser/client/parties/views/parties-list.ng.html',
+          controller: 'PartiesListCtrl'
+        }
+      }
+    });
 });
