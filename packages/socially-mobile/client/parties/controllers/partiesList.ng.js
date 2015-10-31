6fe812afb1a7e90792098edc386cd87dbc1d0384
Dotan Simha <dotansimha@gmail.com>
2015-10-31 21:28:35 +0200

    Step 22.20: Add getMainImageUrl method to the controller

--- packages/socially-mobile/client/parties/controllers/partiesList.ng.js
+++ packages/socially-mobile/client/parties/controllers/partiesList.ng.js
@@ -1,4 +1,4 @@
-angular.module("socially.mobile").controller("PartiesListCtrl", function ($scope, $meteor) {
+angular.module("socially.mobile").controller("PartiesListCtrl", function ($scope, $meteor, $filter) {
   $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
   $scope.images = $meteor.collectionFS(Images, false, Images).subscribe('images');
 
@@ -9,4 +9,12 @@ angular.module("socially.mobile").controller("PartiesListCtrl", function ($scope
   $meteor.autorun($scope, function() {
     $meteor.subscribe('parties', {});
   });
+
+  $scope.getMainImageUrl = function(images) {
+    if (images && images.length && images[0] && images[0].id) {
+      var url = $filter('filter')($scope.images, {_id: images[0].id})[0].url();
+
+      return url;
+    }
+  };
 });
