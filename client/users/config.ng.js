8f5a9980023b911f03273b4b6bcac4141c86b724
Dotan Simha <dotansimha@gmail.com>
2015-11-02 21:19:44 +0200

    Step 18.21: Add icon configuration

---
+++ client/users/config.ng.js
@@ -0,0 +1,9 @@
+angular.module('socially').config(
+  function ($mdIconProvider) {
+
+    //configure social icons for registration
+    $mdIconProvider
+      .icon('facebook','icons/facebook.svg')
+      .icon('twitter','icons/twitter.svg')
+      .icon('google-plus','icons/google-plus.svg')
+  });
