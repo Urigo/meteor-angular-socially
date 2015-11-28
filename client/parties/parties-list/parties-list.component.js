6245ea5f5324d535ebc066855c8d38d3780c4930
Dotan Simha <dotansimha@gmail.com>
2015-11-28 20:49:47 +0200

    Step 7.5: client/parties/parties-list/parties-list.component.js

---
+++ client/parties/parties-list/parties-list.component.js
@@ -0,0 +1,27 @@
+angular.module('socially').directive('partiesList', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'parties-list.html',
+    controllerAs: 'partiesList',
+    controller: function ($scope, $reactive) {
+      $reactive(this).attach($scope);
+
+      this.newParty = {};
+
+      this.helpers({
+        parties: () => {
+          return Parties.find({});
+        }
+      });
+
+      this.addParty = () => {
+        Parties.insert(this.newParty);
+        this.newParty = {};
+      };
+
+      this.removeParty = (party) => {
+        Parties.remove({_id: party._id});
+      }
+    }
+  }
+});
