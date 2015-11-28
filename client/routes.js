c4029c2c4c97dcbed282bca496364cbeb0e3c478
Dotan Simha <dotansimha@gmail.com>
2015-11-28 21:48:35 +0200

    Step 8.10: Added the ability to detect if the user is not logged in

--- client/routes.js
+++ client/routes.js
@@ -1,25 +1,33 @@
-angular.module('socially').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
+angular.module('socially')
+  .config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
 
-  $stateProvider
-    .state('parties', {
-      url: '/parties',
-      template: '<parties-list></parties-list>'
-    })
-    .state('partyDetails', {
-      url: '/parties/:partyId',
-      template: '<party-details></party-details>',
-      resolve: {
-        currentUser: ($q) => {
-          if (Meteor.userId() == null) {
-            return $q.reject();
-          }
-          else {
-            return $q.resolve();
+    $stateProvider
+      .state('parties', {
+        url: '/parties',
+        template: '<parties-list></parties-list>'
+      })
+      .state('partyDetails', {
+        url: '/parties/:partyId',
+        template: '<party-details></party-details>',
+        resolve: {
+          currentUser: ($q) => {
+            if (Meteor.userId() == null) {
+              return $q.reject('AUTH_REQUIRED');
+            }
+            else {
+              return $q.resolve();
+            }
           }
         }
+      });
+
+    $urlRouterProvider.otherwise("/parties");
+  })
+  .run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('parties');
       }
     });
-
-  $urlRouterProvider.otherwise("/parties");
-});
+  });
