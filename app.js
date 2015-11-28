797cad8b6f663298aca5f66a6a0af95153c10f41
Dotan Simha <dotansimha@gmail.com>
2015-11-28 17:06:45 +0200

    Step 5.6: Add party details state in the routes file

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ if (Meteor.isClient) {
       .state('parties', {
         url: '/parties',
         template: '<parties-list></parties-list>'
+      })
+      .state('partyDetails', {
+        url: '/parties/:partyId',
+        template: '<party-details></party-details>'
       });
 
     $urlRouterProvider.otherwise("/parties");
