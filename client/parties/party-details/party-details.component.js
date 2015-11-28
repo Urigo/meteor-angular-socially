b10b82cbdb1d35d07f4260cdf2083aeb4a16e268
Dotan Simha <dotansimha@gmail.com>
2015-11-28 20:51:16 +0200

    Step 7.7: client/parties/party-details/party-details.component.js

---
+++ client/parties/party-details/party-details.component.js
@@ -0,0 +1,32 @@
+angular.module('socially').directive('partyDetails', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'party-details.html',
+    controllerAs: 'partyDetails',
+    controller: function ($scope, $stateParams, $reactive) {
+      $reactive(this).attach($scope);
+
+      this.helpers({
+        party: () => {
+          return Parties.findOne({_id: $stateParams.partyId});
+        }
+      });
+
+      this.save = () => {
+        Parties.update({_id: $stateParams.partyId}, {
+          $set: {
+            name: this.party.name,
+            description: this.party.description
+          }
+        }, (error) => {
+          if (error) {
+            console.log('Oops, unable to update the party...');
+          }
+          else {
+            console.log('Done!');
+          }
+        });
+      };
+    }
+  }
+});
