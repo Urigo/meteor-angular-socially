be9a61a85417207417e89722b3ab43cf56cf33d5
Dotan Simha <dotansimha@gmail.com>
2015-11-28 22:15:04 +0200

    Step 11.1: Update the module initialization

--- client/lib/app.js
+++ client/lib/app.js
@@ -3,3 +3,14 @@ angular.module('socially', [
   'ui.router',
   'accounts.ui'
 ]);
+
+function onReady() {
+  angular.bootstrap(document, ['socially'], {
+    strictDi: true
+  });
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
