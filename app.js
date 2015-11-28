dae52cadeb4048bf68565be6b370a91dba51e1ca
Dotan Simha <dotansimha@gmail.com>
2015-11-28 16:00:58 +0200

    Step 3.8: Change the PartiesListCtrl to use component code-style with directive

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ Parties = new Mongo.Collection("parties");
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor']);
 
-  angular.module('socially').controller('PartiesListCtrl', function ($scope, $reactive) {
-    $reactive(this).attach($scope);
+  angular.module('socially').directive('partiesList', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'parties-list.html',
+      controllerAs: 'partiesList',
+      controller: function($scope, $reactive) {
+        $reactive(this).attach($scope);
 
-    this.helpers({
-      parties: () => {
-        return Parties.find({});
+        this.helpers({
+          parties: () => {
+            return Parties.find({});
+          }
+        });
       }
-    });
+    }
   });
 }
