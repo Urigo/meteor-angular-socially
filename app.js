dfaad36fe609ea0abcf59bb84f8fab3bcfc84646
Dotan Simha <dotansimha@gmail.com>
2015-11-28 15:50:54 +0200

    Step 3.5: Change the parties controller to use $reactive and `this` instead of `$scope`

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ Parties = new Mongo.Collection("parties");
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor']);
 
-  angular.module('socially').controller('PartiesListCtrl', function ($scope) {
-    $scope.helpers({
+  angular.module('socially').controller('PartiesListCtrl', function ($scope, $reactive) {
+    $reactive(this).attach($scope);
+
+    this.helpers({
       parties: () => {
         return Parties.find({});
       }
