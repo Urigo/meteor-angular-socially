15a2dbcc81b28b7d1603b766192b85e9f62dbb73
Dotan Simha <dotansimha@gmail.com>
2015-11-28 21:09:02 +0200

    Step 8.6: Add allow logic to the model

--- model/parties.js
+++ model/parties.js
@@ -1,1 +1,13 @@
 Parties = new Mongo.Collection("parties");
+
+Parties.allow({
+  insert: function (userId, party) {
+    return userId && party.owner === userId;
+  },
+  update: function (userId, party, fields, modifier) {
+    return userId && party.owner === userId;
+  },
+  remove: function (userId, party) {
+    return userId && party.owner === userId;
+  }
+});
