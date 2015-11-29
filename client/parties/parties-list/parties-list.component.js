0633265d281b93bae55f6afcf8e9d3aae3af6928
Dotan Simha <dotansimha@gmail.com>
2015-11-30 00:59:59 +0200

    Step 17.12: Update the component, add the modal opening code and update the map theme

--- client/parties/parties-list/parties-list.component.js
+++ client/parties/parties-list/parties-list.component.js
@@ -3,10 +3,9 @@ angular.module('socially').directive('partiesList', function () {
     restrict: 'E',
     templateUrl: 'client/parties/parties-list/parties-list.html',
     controllerAs: 'partiesList',
-    controller: function ($scope, $reactive) {
+    controller: function ($scope, $reactive, $modal) {
       $reactive(this).attach($scope);
 
-      this.newParty = {};
       this.perPage = 3;
       this.page = 1;
       this.sort = {
@@ -38,6 +37,42 @@ angular.module('socially').directive('partiesList', function () {
           latitude: 45,
           longitude: -73
         },
+        options: {
+          maxZoom: 10,
+          styles: [{
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [{"color": "#444444"}]
+          }, {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [{"color": "#f2f2f2"}]
+          }, {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+          }, {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [{"saturation": -100}, {"lightness": 45}]
+          }, {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}]
+          }, {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [{"visibility": "off"}]
+          }, {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+          }, {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [{"color": "#46bcec"}, {"visibility": "on"}]
+          }]
+        },
         zoom: 8
       };
 
@@ -53,12 +88,6 @@ angular.module('socially').directive('partiesList', function () {
           this.getReactively('searchText')
         ]
       });
-
-      this.addParty = () => {
-        this.newParty.owner = Meteor.user()._id;
-        Parties.insert(this.newParty);
-        this.newParty = {};
-      };
 
       this.removeParty = (party) => {
         Parties.remove({_id: party._id});
@@ -112,6 +141,27 @@ angular.module('socially').directive('partiesList', function () {
           return (_.contains(party.invited, user._id) && !_.findWhere(party.rsvps, {user: user._id}));
         });
       };
+
+      this.openAddNewPartyModal = function () {
+        $modal.open({
+          animation: true,
+          template: '<add-new-party-modal></add-new-party-modal>'
+        });
+      };
+
+      this.isRSVP = (rsvp, party) => {
+        if (Meteor.userId() == null) {
+          return false;
+        }
+
+        let rsvpIndex = party.myRsvpIndex;
+        rsvpIndex = rsvpIndex || _.indexOf(_.pluck(party.rsvps, 'user'), Meteor.userId());
+
+        if (rsvpIndex !== -1) {
+          party.myRsvpIndex = rsvpIndex;
+          return party.rsvps[rsvpIndex].rsvp === rsvp;
+        }
+      }
     }
   }
 });
