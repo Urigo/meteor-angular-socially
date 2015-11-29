1a42864496af0b762171624f8199b150136714a0
Dotan Simha <dotansimha@gmail.com>
2015-11-29 20:56:53 +0200

    Step 14.6: Update the parties subscription to include invited

--- server/parties.js
+++ server/parties.js
@@ -17,6 +17,12 @@ Meteor.publish("parties", function (options, searchString) {
           {owner: this.userId},
           {owner: {$exists: true}}
         ]
+      },
+      {
+        $and: [
+          {invited: this.userId},
+          {invited: {$exists: true}}
+        ]
       }
     ]
   };
