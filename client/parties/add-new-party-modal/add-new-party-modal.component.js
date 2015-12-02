e7d5bba717d21a7c61c5d9b84c9027cda8f99214
Dotan Simha <dotansimha@gmail.com>
2015-12-02 21:41:47 +0200

    Step 20.17: Add the logic for saving the cropped image

--- client/parties/add-new-party-modal/add-new-party-modal.component.js
+++ client/parties/add-new-party-modal/add-new-party-modal.component.js
@@ -16,6 +16,10 @@ angular.module('socially').directive('addNewPartyModal', function () {
 
       this.addNewParty = () => {
         this.newParty.owner = Meteor.userId();
+        this.newParty.images = (this.newParty.images || {}).map((image) => {
+          return image._id;
+        });
+
         Parties.insert(this.newParty);
         this.newParty = {};
         $mdDialog.hide();
@@ -40,6 +44,20 @@ angular.module('socially').directive('addNewPartyModal', function () {
         }
         else {
           this.cropImgSrc = undefined;
+        }
+      };
+
+      this.saveCroppedImage = () => {
+        if (this.myCroppedImage !== '') {
+          Images.insert(this.myCroppedImage, (err, fileObj) => {
+            if (!this.newParty.images) {
+              this.newParty.images = [];
+            }
+
+            this.newParty.images.push(fileObj);
+            this.cropImgSrc = undefined;
+            this.myCroppedImage = '';
+          });
         }
       };
     }
