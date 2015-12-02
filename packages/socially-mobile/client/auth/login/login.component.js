b9f2ed9ad58d0d31d26ab6f9824ae738aca77766
Dotan Simha <dotansimha@gmail.com>
2015-12-02 23:47:36 +0200

    Step 21.17: Implement the mobile login component

--- packages/socially-mobile/client/auth/login/login.component.js
+++ packages/socially-mobile/client/auth/login/login.component.js
@@ -1,10 +1,31 @@
 angular.module("socially.mobile").directive('login', function() {
   return {
     restrict: 'E',
-    templateUrl: 'client/auth/login/login.html',
+    templateUrl: '/packages/socially-mobile/client/auth/login/login.html',
     controllerAs: 'login',
     controller: function ($scope, $reactive, $state) {
       $reactive(this).attach($scope);
+
+      this.isStepTwo = false;
+      this.phoneNumber = '';
+      this.verificationCode = '';
+      this.error = '';
+
+      this.verifyPhone = () => {
+        Accounts.requestPhoneVerification(this.phoneNumber);
+        this.isStepTwo = true;
+      };
+
+      this.verifyCode = () => {
+        Accounts.verifyPhone(this.phoneNumber, this.verificationCode, (err) => {
+          if (err) {
+            this.error = err;
+          }
+          else {
+            $state.go('parties');
+          }
+        });
+      }
     }
   }
 });
