94c92ac5119199a1573c55a4b650d9e8e8bcb28d
Dotan Simha <dotansimha@gmail.com>
2015-12-02 19:41:21 +0200

    Step 18.15: Create the socially component

---
+++ client/socially/socially.component.js
@@ -0,0 +1,19 @@
+angular.module('socially').directive('socially', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'client/socially/socially.html',
+    controllerAs: 'socially',
+    controller: function ($scope, $reactive) {
+      $reactive(this).attach($scope);
+
+      this.helpers({
+        isLoggedIn: () => {
+          return Meteor.userId() !== null;
+        },
+        currentUser: () => {
+          return Meteor.user();
+        }
+      });
+    }
+  }
+});
