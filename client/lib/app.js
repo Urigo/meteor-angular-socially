78e5c9059ebb429f1629c4345583b56cd1d9c0f0
Dotan Simha <dotansimha@gmail.com>
2015-12-02 19:10:43 +0200

    Step 18.10: Add angular material icons definitions

--- client/lib/app.js
+++ client/lib/app.js
@@ -7,6 +7,17 @@ angular.module('socially', [
   'ngMaterial'
 ]);
 
+angular.module('socially').config(($mdIconProvider) => {
+  $mdIconProvider
+    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
+});
+
 function onReady() {
   angular.bootstrap(document, ['socially'], {
     strictDi: true
