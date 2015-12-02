1d416c3bd916d8f10053399ad730c5546db9bb61
Dotan Simha <dotansimha@gmail.com>
2015-12-02 23:40:26 +0200

    Step 21.13: Implement the actual browser login component

--- packages/socially-browser/client/auth/login/login.component.js
+++ packages/socially-browser/client/auth/login/login.component.js
@@ -1,10 +1,28 @@
 angular.module("socially.browser").directive('login', function() {
   return {
     restrict: 'E',
-    templateUrl: 'client/auth/login/login.html',
+    templateUrl: '/packages/socially-browser/client/auth/login/login.html',
     controllerAs: 'login',
     controller: function ($scope, $reactive, $state) {
       $reactive(this).attach($scope);
+
+      this.credentials = {
+        email: '',
+        password: ''
+      };
+
+      this.error = '';
+
+      this.login = () => {
+        Meteor.loginWithPassword(this.credentials.email, this.credentials.password, (err) => {
+          if (err) {
+            this.error = err;
+          }
+          else {
+            $state.go('parties');
+          }
+        });
+      };
     }
   }
 });
