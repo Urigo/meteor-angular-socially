b4aa25172c566aa96f197a833187b285d6d1ad64
Dotan Simha <dotansimha@gmail.com>
2015-12-05 21:13:58 +0200

    Step 22.6: Add driftyco:ionic package

--- packages/socially-mobile/package.js
+++ packages/socially-mobile/package.js
@@ -13,6 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('angular');
+  api.use('driftyco:ionic');
 
   api.addFiles([
     'client/lib/module.js',
