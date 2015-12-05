fec128a6a0163fc9ce8269e834c635b8349cb7c1
Dotan Simha <dotansimha@gmail.com>
2015-12-05 21:13:02 +0200

    Step 22.4: Update the package.js of the brower package

--- packages/socially-browser/package.js
+++ packages/socially-browser/package.js
@@ -13,13 +13,30 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('angular');
+  api.use('less');
 
   api.addFiles([
     'client/lib/module.js',
     'client/auth/login/login.component.js',
     'client/auth/login/login.html',
+    'client/auth/register/register.component.js',
+    'client/auth/register/register.html',
+    'client/auth/reset-password/reset-password.component.js',
+    'client/auth/reset-password/reset-password.html',
     'client/socially/socially.component.js',
-    'client/socially/socially.html'
+    'client/socially/socially.html',
+    'client/parties/add-new-party-modal/add-new-party-modal.component.js',
+    'client/parties/add-new-party-modal/add-new-party-modal.html',
+    'client/parties/parties-list/parties-list.component.js',
+    'client/parties/parties-list/parties-list.html',
+    'client/parties/party-details/party-details.component.js',
+    'client/parties/party-details/party-details.html',
+    'client/parties/styles/google-maps.css',
+    'client/styles/navbar.import.less',
+    'client/parties/party-details/party-details.import.less',
+    'client/parties/parties-list/parties-list.import.less',
+    'client/parties/add-new-party-modal/add-new-party-modal.import.less',
+    'client/styles/main.less'
   ], 'client');
 });
 
