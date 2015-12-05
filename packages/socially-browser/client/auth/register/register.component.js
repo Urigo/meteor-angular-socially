68d8fc8a772f6d45036355e8c9af10fdc39fdb6a
Dotan Simha <dotansimha@gmail.com>
2015-12-05 21:12:48 +0200

    Step 22.3: Move all browser components to socially-browser package

--- packages/socially-browser/client/auth/register/register.component.js
+++ packages/socially-browser/client/auth/register/register.component.js
@@ -1,19 +1,20 @@
-angular.module("socially").directive('resetpw', function() {
+angular.module("socially.browser").directive('register', function() {
   return {
     restrict: 'E',
-    templateUrl: 'client/auth/reset-password/reset-password.html',
-    controllerAs: 'resetpw',
+    templateUrl: '/packages/socially-browser/client/auth/register/register.html',
+    controllerAs: 'register',
     controller: function ($scope, $reactive, $state) {
       $reactive(this).attach($scope);
 
       this.credentials = {
-        email: ''
+        email: '',
+        password: ''
       };
 
       this.error = '';
 
-      this.reset = () => {
-        Accounts.forgotPassword(this.credentials, (err) => {
+      this.register = () => {
+        Accounts.createUser(this.credentials, (err) => {
           if (err) {
             this.error = err;
           }
--- packages/socially-browser/client/auth/reset-password/reset-password.component.js
+++ packages/socially-browser/client/auth/reset-password/reset-password.component.js
@@ -1,20 +1,19 @@
-angular.module("socially").directive('register', function() {
+angular.module("socially.browser").directive('resetpw', function() {
   return {
     restrict: 'E',
-    templateUrl: 'client/auth/register/register.html',
-    controllerAs: 'register',
+    templateUrl: '/packages/socially-browser/client/auth/reset-password/reset-password.html',
+    controllerAs: 'resetpw',
     controller: function ($scope, $reactive, $state) {
       $reactive(this).attach($scope);
 
       this.credentials = {
-        email: '',
-        password: ''
+        email: ''
       };
 
       this.error = '';
 
-      this.register = () => {
-        Accounts.createUser(this.credentials, (err) => {
+      this.reset = () => {
+        Accounts.forgotPassword(this.credentials, (err) => {
           if (err) {
             this.error = err;
           }
