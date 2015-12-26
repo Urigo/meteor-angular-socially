1a4a875e934d73d17c7cf3cb5154b9653d270b07
Dotan Simha <dotansimha@gmail.com>
2015-12-26 13:38:18 +0200

    Step 23.2: Change the syntax to use angular2-now

--- client/socially/socially.component.js
+++ client/socially/socially.component.js
@@ -1,30 +1,32 @@
-angular.module('socially').directive('socially', function () {
-  return {
-    restrict: 'E',
-    templateUrl: () => {
-      if (Meteor.isCordova) {
-        return '/packages/socially-mobile/client/socially/socially.html';
-      }
-      else {
-        return '/packages/socially-browser/client/socially/socially.html';
-      }
-    },
-    controllerAs: 'socially',
-    controller: function ($scope, $reactive) {
-      $reactive(this).attach($scope);
+let {Component, View, SetModule, Inject} = angular2now;
 
-      this.helpers({
-        isLoggedIn: () => {
-          return Meteor.userId() !== null;
-        },
-        currentUser: () => {
-          return Meteor.user();
-        }
-      });
+SetModule('socially');
 
-      this.logout = () => {
-        Accounts.logout();
+@Component({selector: 'socially'})
+@View({templateUrl: () => {
+  if (Meteor.isCordova) {
+    return '/packages/socially-mobile/client/socially/socially.html';
+  }
+  else {
+    return '/packages/socially-browser/client/socially/socially.html';
+  }
+}})
+@Inject('$reactive', '$scope')
+class socially {
+  constructor($reactive, $scope) {
+    $reactive(this).attach($scope);
+
+    this.helpers({
+      isLoggedIn: () => {
+        return Meteor.userId() !== null;
+      },
+      currentUser: () => {
+        return Meteor.user();
       }
+    });
+
+    this.logout = () => {
+      Accounts.logout();
     }
   }
-});
+}
