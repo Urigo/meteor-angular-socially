d255dd26fa89e169f25ecfc7331fbfe8e75dd8be
Dotan Simha <dotansimha@gmail.com>
2015-12-26 14:14:19 +0200

    Step 23.8: Update parties list component to be real class

--- client/parties/parties-list/parties-list.component.js
+++ client/parties/parties-list/parties-list.component.js
@@ -25,28 +25,145 @@ class partiesList {
     this.searchText = '';
 
     this.helpers({
-      parties: () => {
-        return Parties.find({}, {sort: this.getReactively('sort')});
-      },
-      users: () => {
-        return Meteor.users.find({});
-      },
-      partiesCount: () => {
-        return Counts.get('numberOfParties');
-      },
-      isLoggedIn: () => {
-        return Meteor.userId() !== null;
-      },
-      currentUserId: () => {
-        return Meteor.userId();
-      },
-      images: () => {
-        return Images.find({});
-      }
+      parties: this._partiesCollection,
+      users: this._usersCollection,
+      partiesCount: this._partiesCount,
+      isLoggedIn: this._isLoggedIn,
+      currentUserId: this._currentUserId,
+      images: this._imagesCollection
     });
 
     this.subscribe('images');
+    this.subscribe('users');
+    this.subscribe('parties', this._partiesSubscription.bind(this));
+    this.defineMapProperties();
+  }
 
+  _imagesCollection() {
+    return Images.find({});
+  }
+
+  _currentUserId() {
+    return Meteor.userId();
+  }
+
+  _isLoggedIn() {
+    return Meteor.userId() !== null;
+  }
+
+  _partiesCount() {
+    return Counts.get('numberOfParties');
+  }
+
+  _usersCollection() {
+    return Meteor.users.find({});
+  }
+
+  _partiesCollection() {
+    return Parties.find({}, {sort: this.getReactively('sort')});
+  }
+
+  _partiesSubscription() {
+    return [
+      {
+        limit: parseInt(this.perPage),
+        skip: parseInt((this.getReactively('page') - 1) * this.perPage),
+        sort: this.getReactively('sort')
+      },
+      this.getReactively('searchText')
+    ]
+  }
+
+  removeParty(party) {
+    Parties.remove({_id: party._id});
+  }
+
+  pageChanged(newPage) {
+    this.page = newPage;
+  }
+
+  updateSort() {
+    this.sort = {
+      name: parseInt(this.orderProperty)
+    }
+  }
+
+  getPartyCreator(party) {
+    if (!party) {
+      return '';
+    }
+
+    let owner = Meteor.users.findOne(party.owner);
+
+    if (!owner) {
+      return 'nobody';
+    }
+
+    if (Meteor.userId() !== null && owner._id === Meteor.userId()) {
+      return 'me';
+    }
+
+    return owner;
+  }
+
+  rsvp(partyId, rsvp) {
+    Meteor.call('rsvp', partyId, rsvp, (error) => {
+      if (error) {
+        console.log('Oops, unable to rsvp!');
+      }
+      else {
+        console.log('RSVP Done!');
+      }
+    });
+  }
+
+  getUserById(userId) {
+    return Meteor.users.findOne(userId);
+  }
+
+  outstandingInvitations(party) {
+    return _.filter(this.users, (user) => {
+      return (_.contains(party.invited, user._id) && !_.findWhere(party.rsvps, {user: user._id}));
+    });
+  }
+
+  openAddNewPartyModal() {
+    $mdDialog.show({
+      template: '<add-new-party-modal></add-new-party-modal>',
+      clickOutsideToClose: true
+    });
+  }
+
+  isRSVP(rsvp, party) {
+    if (Meteor.userId() == null) {
+      return false;
+    }
+
+    let rsvpIndex = party.myRsvpIndex;
+    rsvpIndex = rsvpIndex || _.indexOf(_.pluck(party.rsvps, 'user'), Meteor.userId());
+
+    if (rsvpIndex !== -1) {
+      party.myRsvpIndex = rsvpIndex;
+
+      return party.rsvps[rsvpIndex].rsvp === rsvp;
+    }
+  }
+
+  getMainImage(images, onlyUrl) {
+    if (images && images.length && images[0] && images[0]) {
+      var url = $filter('filter')(this.images, {_id: images[0]})[0].url();
+
+      if (onlyUrl) {
+        return url;
+      }
+
+      return {
+        'background-image': 'url("' + url + '")'
+      }
+    }
+  }
+
+  defineMapProperties() {
     this.map = {
       center: {
         latitude: 45,
@@ -89,107 +206,6 @@ class partiesList {
         }]
       },
       zoom: 8
-    };
-
-    this.subscribe('users');
-
-    this.subscribe('parties', () => {
-      return [
-        {
-          limit: parseInt(this.perPage),
-          skip: parseInt((this.getReactively('page') - 1) * this.perPage),
-          sort: this.getReactively('sort')
-        },
-        this.getReactively('searchText')
-      ]
-    });
-
-    this.removeParty = (party) => {
-      Parties.remove({_id: party._id});
-    };
-
-    this.pageChanged = (newPage) => {
-      this.page = newPage;
-    };
-
-    this.updateSort = () => {
-      this.sort = {
-        name: parseInt(this.orderProperty)
-      }
-    };
-
-    this.getPartyCreator = function (party) {
-      if (!party) {
-        return '';
-      }
-
-      let owner = Meteor.users.findOne(party.owner);
-
-      if (!owner) {
-        return 'nobody';
-      }
-
-      if (Meteor.userId() !== null && owner._id === Meteor.userId()) {
-        return 'me';
-      }
-
-      return owner;
-    };
-
-    this.rsvp = (partyId, rsvp) => {
-      Meteor.call('rsvp', partyId, rsvp, (error) => {
-        if (error) {
-          console.log('Oops, unable to rsvp!');
-        }
-        else {
-          console.log('RSVP Done!');
-        }
-      });
-    };
-
-    this.getUserById = (userId) => {
-      return Meteor.users.findOne(userId);
-    };
-
-    this.outstandingInvitations = (party) => {
-      return _.filter(this.users, (user) => {
-        return (_.contains(party.invited, user._id) && !_.findWhere(party.rsvps, {user: user._id}));
-      });
-    };
-
-    this.openAddNewPartyModal = function () {
-      $mdDialog.show({
-        template: '<add-new-party-modal></add-new-party-modal>',
-        clickOutsideToClose: true
-      });
-    };
-
-    this.isRSVP = (rsvp, party) => {
-      if (Meteor.userId() == null) {
-        return false;
-      }
-
-      let rsvpIndex = party.myRsvpIndex;
-      rsvpIndex = rsvpIndex || _.indexOf(_.pluck(party.rsvps, 'user'), Meteor.userId());
-
-      if (rsvpIndex !== -1) {
-        party.myRsvpIndex = rsvpIndex;
-        return party.rsvps[rsvpIndex].rsvp === rsvp;
-      }
-    };
-
-    this.getMainImage = (images, onlyUrl) => {
-      if (images && images.length && images[0] && images[0]) {
-        var url = $filter('filter')(this.images, {_id: images[0]})[0].url();
-
-        if (onlyUrl) {
-          return url;
-        }
-
-        return {
-          'background-image': 'url("' + url + '")'
-        }
-      }
     };
   }
 }
