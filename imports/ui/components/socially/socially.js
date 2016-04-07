dd14e42104bde7c4b7a04aa2d62e3d75cf6445fd
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 11:10:34 +0200

    Step 5.2: Add uiRouter to Socially

--- imports/ui/components/socially/socially.js
+++ imports/ui/components/socially/socially.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 
 import template from './socially.html';
 import { name as PartiesList } from '../partiesList/partiesList';
@@ -11,6 +12,7 @@ const name = 'socially';
 // create a module
 export default angular.module(name, [
   angularMeteor,
+  uiRouter,
   PartiesList
 ]).component(name, {
   template,
