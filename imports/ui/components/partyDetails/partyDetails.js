69863ddcf79f975809b24ac48608ef614b3c51b7
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 12:05:29 +0200

    Step 8.9: Add permissions limit to the party details page

--- imports/ui/components/partyDetails/partyDetails.js
+++ imports/ui/components/partyDetails/partyDetails.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 
+import { Meteor } from 'meteor/meteor';
+
 import template from './partyDetails.html';
 import { Parties } from '../../../api/parties';
 
@@ -58,6 +60,15 @@ function config($stateProvider) {
 
   $stateProvider.state('partyDetails', {
     url: '/parties/:partyId',
-    template: '<party-details></party-details>'
+    template: '<party-details></party-details>',
+    resolve: {
+      currentUser($q) {
+        if (Meteor.userId() === null) {
+          return $q.reject();
+        } else {
+          return $q.resolve();
+        }
+      }
+    }
   });
 }
