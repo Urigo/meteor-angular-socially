4016e79d970179dd5c526a67822779f446a2a413
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 10:34:53 +0200

    Step 3.18: Import Socially in main.js

--- client/main.js
+++ client/main.js
@@ -1,9 +1,4 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-import { name as PartiesList } from '../imports/ui/components/partiesList/partiesList';
-
-angular.module('socially', [
-    angularMeteor,
-    PartiesList
-  ]);
+import { name as Socially } from '../imports/ui/components/socially/socially';
