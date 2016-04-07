5a28b98914c9b74da007c4a7a02f2e45c9eb0d8e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 10:47:30 +0200

    Step 4.7: Add submit method

--- imports/ui/components/partyAdd/partyAdd.js
+++ imports/ui/components/partyAdd/partyAdd.js
@@ -3,7 +3,15 @@ import angularMeteor from 'angular-meteor';
 
 import './partyAdd.html';
 
-class PartyAdd {}
+class PartyAdd {
+  constructor() {
+    this.party = {};
+  }
+
+  submit() {
+    console.log('submit:', this.party);
+  }
+}
 
 const name = 'partyAdd';
 
