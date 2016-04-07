0c9a5e36562ca4cd97718f32bc7c693df29878f2
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 09:50:30 +0200

    Step 2.2: Add the parties controller

--- client/main.js
+++ client/main.js
@@ -2,5 +2,17 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
 angular.module('socially', [
-  angularMeteor
-]);
+    angularMeteor
+  ])
+  .controller('PartiesListCtrl', ['$scope', function($scope) {
+    $scope.parties = [{
+      'name': 'Dubstep-Free Zone',
+      'description': 'Can we please just for an evening not listen to dubstep.'
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!'
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.'
+    }];
+  }]);
