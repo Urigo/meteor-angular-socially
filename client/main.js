b75b29c666d97317ca89ce2198fcdfcb5c5a71af
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 09:56:46 +0200

    Step 3.2: Add the parties collection helper

--- client/main.js
+++ client/main.js
@@ -1,20 +1,16 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Parties } from '../collections/parties';
 
 angular.module('socially', [
     angularMeteor
   ])
   .controller('PartiesListCtrl', function($scope) {
     'ngInject';
-    
-    $scope.parties = [{
-      'name': 'Dubstep-Free Zone',
-      'description': 'Can we please just for an evening not listen to dubstep.'
-    }, {
-      'name': 'All dubstep all the time',
-      'description': 'Get it on!'
-    }, {
-      'name': 'Savage lounging',
-      'description': 'Leisure suit required. And only fiercest manners.'
-    }];
+
+    $scope.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    });
   });
