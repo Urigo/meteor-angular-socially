b93e344ebff255f6a01bbd18b68b66db5a454f83
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 12:48:25 +0200

    Step 12.14: Create PartiesSort component

---
+++ imports/ui/components/partiesSort/partiesSort.js
@@ -0,0 +1,34 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import template from './partiesSort.html';
+
+class PartiesSort {
+  constructor() {
+    this.changed();
+  }
+
+  changed() {
+    this.onChange({
+      sort: {
+        [this.property]: parseInt(this.order)
+      }
+    });
+  }
+}
+
+const name = 'partiesSort';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor
+]).component(name, {
+  template,
+  bindings: {
+    onChange: '&',
+    property: '@',
+    order: '@'
+  },
+  controllerAs: name,
+  controller: PartiesSort
+});
