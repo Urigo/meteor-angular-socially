19ba94dc7a1f4cfa5b01ef29e829cd13d4b40835
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 15:11:57 +0200

    Step 6.6: Tests of PartyDetails

---
+++ imports/ui/components/partyDetails/client/partyDetails.tests.js
@@ -0,0 +1,49 @@
+import { name as PartyDetails } from '../partyDetails';
+import { Parties } from '../../../../api/parties';
+import 'angular-mocks';
+
+describe('PartyDetails', () => {
+  beforeEach(() => {
+    window.module(PartyDetails);
+  });
+
+  describe('controller', () => {
+    let controller;
+    const party = {
+      _id: 'partyId',
+      name: 'Foo',
+      description: 'Birthday of Foo'
+    };
+
+    beforeEach(() => {
+      inject(($rootScope, $componentController) => {
+        controller = $componentController(PartyDetails, {
+          $scope: $rootScope.$new(true)
+        });
+      });
+    });
+
+    describe('save()', () => {
+      beforeEach(() => {
+        spyOn(Parties, 'update');
+        controller.party = party;
+        controller.save();
+      });
+
+      it('should update a proper party', () => {
+        expect(Parties.update.calls.mostRecent().args[0]).toEqual({
+          _id: party._id
+        });
+      });
+
+      it('should update with proper modifier', () => {
+        expect(Parties.update.calls.mostRecent().args[1]).toEqual({
+          $set: {
+            name: party.name,
+            description: party.description
+          }
+        });
+      });
+    });
+  });
+});
