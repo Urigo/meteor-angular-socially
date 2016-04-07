116b4022cb4adb30e715c6d0e539ab9fd770e2d0
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 11:04:08 +0200

    Step 4.14: Create PartyRemove component

---
+++ imports/ui/components/partyRemove/partyRemove.js
@@ -0,0 +1,21 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import template from './partyRemove.html';
+
+class PartyRemove {
+  remove() {
+    console.log('remove party');
+  }
+}
+
+const name = 'partyRemove';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: PartyRemove
+});
