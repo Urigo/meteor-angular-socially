5646d1c49d828f7c5ae40bd598a96a14e1ef67a5
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 10:25:52 +0200

    Step 3.9: Move PartiesList to imports

---
+++ imports/ui/components/partiesList/partiesList.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+class PartiesList {
+  constructor($scope, $reactive) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+
+    this.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    });
+  }
+}
+
+const name = 'partiesList';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor
+]).component(name, {
+  templateUrl: `imports/ui/components/${name}/${name}.html`,
+  controllerAs: name,
+  controller: PartiesList
+});
