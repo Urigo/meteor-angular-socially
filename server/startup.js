918e61256f224de3285a9cd7bfbf2fd085a1cc05
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 10:00:24 +0200

    Step 3.3: Add some data to the parties collection

---
+++ server/startup.js
@@ -0,0 +1,18 @@
+Meteor.startup(() => {
+  if (Parties.find().count() === 0) {
+    const parties = [{
+      'name': 'Dubstep-Free Zone',
+      'description': 'Fast just got faster with Nexus S.'
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!'
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.'
+    }];
+
+    parties.forEach((party) => {
+      Parties.insert(party)
+    });
+  }
+});
