816640b5e3bf98d06cc35b69ffe19e53ace35e1a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 10:07:22 +0200

    Step 3.5: Change the parties controller to use $reactive and `this` instead of `$scope`

--- client/main.js
+++ client/main.js
@@ -5,10 +5,12 @@ import { Parties } from '../collections/parties';
 angular.module('socially', [
     angularMeteor
   ])
-  .controller('PartiesListCtrl', function($scope) {
+  .controller('PartiesListCtrl', function($scope, $reactive) {
     'ngInject';
 
-    $scope.helpers({
+    $reactive(this).attach($scope);
+
+    this.helpers({
       parties() {
         return Parties.find({});
       }
