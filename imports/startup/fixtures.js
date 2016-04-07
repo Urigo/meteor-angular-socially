7327ad3993827ed13cf8882f0acd475555532c36
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 11:38:12 +0200

    Step 7.1: Move startup to imports

---
+++ imports/startup/fixtures.js
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+import { Parties } from '../api/parties';
+
+Meteor.startup(() => {
+  if (Parties.find().count() === 0) {
+    const parties = [{
+      'name': 'Dubstep-Free Zone',
+      'description': 'Fast just got faster with Nexus S.'
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!'
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.'
+    }];
+
+    parties.forEach((party) => {
+      Parties.insert(party)
+    });
+  }
+});
