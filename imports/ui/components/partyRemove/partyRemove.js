834ae4eaf8d2d762ebfba26ac53f927df36ad053
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 11:06:30 +0200

    Step 4.16: Remove party from collection

--- imports/ui/components/partyRemove/partyRemove.js
+++ imports/ui/components/partyRemove/partyRemove.js
@@ -2,10 +2,13 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
 import template from './partyRemove.html';
+import { Parties } from '../../../api/parties';
 
 class PartyRemove {
   remove() {
-    console.log('remove party');
+    if (this.party) {
+      Parties.remove(this.party._id);
+    }
   }
 }
 
