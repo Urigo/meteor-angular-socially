f4ac7dd0fd289af51aab9aeeb9c33598cc541f1a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 20:28:08 +0200

    Step 16.6: Import styles

--- imports/ui/components/partyMap/partyMap.js
+++ imports/ui/components/partyMap/partyMap.js
@@ -3,6 +3,7 @@ import angularMeteor from 'angular-meteor';
 import 'angular-simple-logger';
 import 'angular-google-maps';
 
+import './partyMap.css';
 import template from './partyMap.html';
 
 class PartyMap {
