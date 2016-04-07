e81d6c9ca481ce73c4ef1041d274993cabc29eca
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 10:42:22 +0200

    Step 4.4: Add PartyAdd to PartiesList

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
 import './partiesList.html';
+import { name as PartyAdd } from '../partyAdd/partyAdd';
 
 class PartiesList {
   constructor($scope, $reactive) {
@@ -21,7 +22,8 @@ const name = 'partiesList';
 
 // create a module
 export default angular.module(name, [
-  angularMeteor
+  angularMeteor,
+  PartyAdd
 ]).component(name, {
   templateUrl: `imports/ui/components/${name}/${name}.html`,
   controllerAs: name,
