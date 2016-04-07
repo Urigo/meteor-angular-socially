73c907b7a52cde8c2a4511e5ee72fe1232dafd39
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 09:56:46 +0200

    Step 3.2: Add the parties collection helper

--- client/main.js
+++ client/main.js
@@ -6,15 +6,10 @@ angular.module('socially', [
   ])
   .controller('PartiesListCtrl', function($scope) {
     'ngInject';
-    
-    $scope.parties = [{
-      'name': 'Dubstep-Free Zone',
-      'description': 'Can we please just for an evening not listen to dubstep.'
-    }, {
-      'name': 'All dubstep all the time',
-      'description': 'Get it on!'
-    }, {
-      'name': 'Savage lounging',
-      'description': 'Leisure suit required. And only fiercest manners.'
-    }];
+
+    $scope.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    });
   });
