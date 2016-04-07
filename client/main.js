7226ae4ba8a242ace589e790fc628ea830d9da67
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 12:29:01 +0200

    Step 11.1: Update the module initialization

--- client/main.js
+++ client/main.js
@@ -1,4 +1,19 @@
 import angular from 'angular';
-import angularMeteor from 'angular-meteor';
+
+import { Meteor } from 'meteor/meteor';
 
 import { name as Socially } from '../imports/ui/components/socially/socially';
+
+function onReady() {
+  angular.bootstrap(document, [
+    Socially
+  ], {
+    strictDi: true
+  });
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
