50c3b23335f087f82eb6017dc03e68b1de485bba
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-07 20:29:34 +0200

    Step 16.7: Add as a dependency to PartyDetails

--- imports/ui/components/partyDetails/partyDetails.js
+++ imports/ui/components/partyDetails/partyDetails.js
@@ -7,6 +7,7 @@ import { Meteor } from 'meteor/meteor';
 import template from './partyDetails.html';
 import { Parties } from '../../../api/parties';
 import { name as PartyUninvited } from '../partyUninvited/partyUninvited';
+import { name as PartyMap } from '../partyMap/partyMap';
 
 class PartyDetails {
   constructor($stateParams, $scope, $reactive) {
@@ -67,7 +68,8 @@ const name = 'partyDetails';
 export default angular.module(name, [
   angularMeteor,
   uiRouter,
-  PartyUninvited
+  PartyUninvited,
+  PartyMap
 ]).component(name, {
   template,
   controllerAs: name,
