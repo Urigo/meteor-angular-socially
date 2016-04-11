ce53ba42f20ebb5a43b59455ddcececf9f139e99
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-11 03:48:03 +0200

    Step 18.49: Create Password

---
+++ imports/ui/components/password/password.js
@@ -0,0 +1,56 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import { Accounts } from 'meteor/accounts-base';
+
+import template from './password.html';
+
+class Register {
+  constructor($scope, $reactive, $state) {
+    'ngInject';
+
+    this.$state = $state;
+
+    $reactive(this).attach($scope);
+
+    this.credentials = {
+      email: ''
+    };
+
+    this.error = '';
+  }
+
+  reset() {
+    Accounts.forgotPassword(this.credentials, this.$bindToContext((err) => {
+      if (err) {
+        this.error = err;
+      } else {
+        this.$state.go('parties');
+      }
+    }));
+  }
+}
+
+const name = 'password';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter
+])
+  .component(name, {
+    template,
+    controllerAs: name,
+    controller: Register
+  })
+  .config(config);
+
+function config($stateProvider) {
+  'ngInject';
+
+  $stateProvider.state('password', {
+    url: '/password',
+    template: '<password></password>'
+  });
+}
