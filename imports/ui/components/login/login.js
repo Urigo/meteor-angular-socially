f346fa26b2d867f7f24af73615a74ad2ea7d281e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-11 03:44:02 +0200

    Step 18.43: Create Login

---
+++ imports/ui/components/login/login.js
@@ -0,0 +1,61 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import { Meteor } from 'meteor/meteor';
+
+import template from './login.html';
+
+import { name as Register } from '../register/register';
+
+class Login {
+  constructor($scope, $reactive, $state) {
+    'ngInject';
+
+    this.$state = $state;
+
+    $reactive(this).attach($scope);
+
+    this.credentials = {
+      email: '',
+      password: ''
+    };
+
+    this.error = '';
+  }
+
+  login() {
+    Meteor.loginWithPassword(this.credentials.email, this.credentials.password,
+      this.$bindToContext((err) => {
+        if (err) {
+          this.error = err;
+        } else {
+          this.$state.go('parties');
+        }
+      })
+    );
+  }
+}
+
+const name = 'login';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter
+])
+  .component(name, {
+    template,
+    controllerAs: name,
+    controller: Login
+  })
+  .config(config);
+
+function config($stateProvider) {
+  'ngInject';
+
+  $stateProvider.state('login', {
+    url: '/login',
+    template: '<login></login>'
+  });
+}
