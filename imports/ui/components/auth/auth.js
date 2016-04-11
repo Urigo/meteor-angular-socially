67c1354dece9fff8799063d1c21b88da63bbef04
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-11 03:25:49 +0200

    Step 18.41: Create Auth component

---
+++ imports/ui/components/auth/auth.js
@@ -0,0 +1,41 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+import template from './auth.html';
+import { name as DisplayNameFilter } from '../../filters/displayNameFilter';
+
+const name = 'auth';
+
+class Auth {
+  constructor($scope, $reactive) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+
+    this.helpers({
+      isLoggedIn() {
+        return !!Meteor.userId();
+      },
+      currentUser() {
+        return Meteor.user();
+      }
+    });
+  }
+
+  logout() {
+    Accounts.logout();
+  }
+}
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  DisplayNameFilter
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: Auth
+});
