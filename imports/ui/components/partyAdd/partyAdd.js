683f7cdeff44a1197732d4075338d30d858f12c1
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-13 11:47:55 +0200

    Step 18.36: Implement `done` expression to PartyAdd

--- imports/ui/components/partyAdd/partyAdd.js
+++ imports/ui/components/partyAdd/partyAdd.js
@@ -14,6 +14,11 @@ class PartyAdd {
   submit() {
     this.party.owner = Meteor.user()._id;
     Parties.insert(this.party);
+
+    if(this.done) {
+      this.done();
+    }
+
     this.reset();
   }
 
@@ -29,6 +34,9 @@ export default angular.module(name, [
   angularMeteor
 ]).component(name, {
   template,
+  bindings: {
+    done: '&?'
+  },
   controllerAs: name,
   controller: PartyAdd
 });
