e25418b222e680dfbb65d4933255b7123d731c45
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-14 20:56:24 +0200

    Step 21.9: Add phone verification method

--- imports/ui/components/login/mobile.js
+++ imports/ui/components/login/mobile.js
@@ -1,1 +1,27 @@
-export class Login {}
+import { Accounts } from 'meteor/accounts-base';
+
+export class Login {
+  constructor($scope, $reactive) {
+    'ngInject';
+
+    $reactive(this).attach($scope);
+
+    this.isStepTwo = false;
+    this.phoneNumber = '';
+    this.verificationCode = '';
+    this.error = '';
+  }
+
+  verifyPhone() {
+    Accounts.requestPhoneVerification(this.phoneNumber, this.$bindToContext((err) => {
+      if (err) {
+        // display also reason of Meteor.Error
+        this.error = err.reason || err;
+      } else {
+        this.error = '';
+        // move to code verification
+        this.isStepTwo = true;
+      }
+    }));
+  }
+}
