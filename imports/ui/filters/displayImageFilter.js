d20dd72538b8ed85ea1ada245f634fcfa2c7ed26
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-25 15:25:39 +0200

    Step 22.16: Create DisplayImageFilter

---
+++ imports/ui/filters/displayImageFilter.js
@@ -0,0 +1,26 @@
+import angular from 'angular';
+
+import { Meteor } from 'meteor/meteor';
+
+const name = 'displayImageFilter';
+
+function DisplayImageFilter(image) {
+  if (!image) {
+    return image;
+  }
+
+  // leave it as it is for the web view
+  if (!Meteor.isCordova) {
+    return image.url;
+  }
+
+  // create new path of an image
+  const path = `ufs/${image.store}/${image._id}/${image.name}`;
+  return Meteor.absoluteUrl(path);
+}
+
+// create a module
+export default angular.module(name, [])
+  .filter(name, () => {
+    return DisplayImageFilter;
+  });
