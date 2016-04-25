2ae5eadb84e698e3575f3b55621d0b8a0fa036e7
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-04-25 11:08:38 +0200

    Step 22.3: ngSanitize as a dependency

--- imports/ui/components/socially/socially.js
+++ imports/ui/components/socially/socially.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import ngMaterial from 'angular-material';
+import ngSanitize from 'angular-sanitize';
 import uiRouter from 'angular-ui-router';
 
 import template from './socially.html';
@@ -17,6 +18,7 @@ const name = 'socially';
 export default angular.module(name, [
   angularMeteor,
   ngMaterial,
+  ngSanitize,
   uiRouter,
   PartiesList,
   PartyDetails,
